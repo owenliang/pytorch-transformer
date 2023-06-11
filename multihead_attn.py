6f489b79b809bc216a28e4633025b0d692d22935
owenliang <owenliang1990@gmail.com>
2023-06-11 11:26:59 +0800

    1

--- multihead_attn.py
+++ multihead_attn.py
@@ -5,25 +5,42 @@ from torch import nn
 import torch 
 from dataset import de_vocab,de_preprocess,train_dataset
 from emb import EmbeddingWithPosition
+import math 
 
 class MultiHeadAttention(nn.Module):
     def __init__(self,emb_size,q_k_size,v_size,head):
         super().__init__()
-        self.w_q=nn.Linear(emb_size,q_k_size)
-        self.w_k=nn.Linear(emb_size,q_k_size)
-        self.w_v=nn.Linear(emb_size,v_size)
+        self.emb_size=emb_size
+        self.q_k_size=q_k_size
+        self.v_size=v_size
+        self.head=head
+
+        self.w_q=nn.Linear(emb_size,q_k_size*head) # 多头
+        self.w_k=nn.Linear(emb_size,q_k_size*head)
+        self.w_v=nn.Linear(emb_size,v_size*head)
 
     def forward(self,x_q,x_k_v,attn_mask):
         # x_q: (batch_size,seq_len,emb_size)
-        q=self.w_q(x_q) # q: (batch_size,seq_len,q_k_size)
-        k=self.w_k(x_k_v) # k: (batch_size,seq_len,q_k_size)
-        attn=torch.matmul(q,k.transpose(1,2)) # (batch_size,seq_len,seq_len) ,  注意力矩阵,row是q,col是k
+        q=self.w_q(x_q) # q: (batch_size,seq_len,q_k_size*head)
+        k=self.w_k(x_k_v) # k: (batch_size,seq_len,q_k_size*head)
         
-        # todo: mask
+        # 多头兼容
+        q=q.view(q.size()[0],q.size()[1],self.head,self.q_k_size).transpose(1,2) # q: (batch_size,head,seq_len,q_k_size)
+        k=k.view(k.size()[0],k.size()[1],self.head,self.q_k_size).transpose(1,2).transpose(2,3) # k:(batch_size,head,q_k_size,seq_len)
+
+        # 注意力矩阵
+        attn=torch.matmul(q,k)/math.sqrt(self.q_k_size) # (batch_size,head,seq_len,seq_len) row是q,col是k
         
-        # todo: head
-        v=self.w_v(x_k_v) # v: (batch_size,seq_len,v_size)
-        z=torch.matmul(attn,v) # z: (batch_size,seq_len,v_size), 多头输出
+        # 注意力分值处理
+        # attn_mask: (batch_size,seq_len,seq_len)
+        attn_mask=attn_mask.unsqueeze(1).expand(-1,self.head,-1,-1) # attn_mask: (batch_size,1,seq_len,seq_len)
+        attn=attn.masked_fill(attn_mask,-torch.inf)
+        attn=torch.softmax(attn,dim=-1) # scores: (batch_size,1,seq_len,seq_len)
+
+        # 注意力与V相乘
+        v=self.w_v(x_k_v) # v: (batch_size,seq_len,v_size*head)
+        v=v.view(v.size()[0],v.size()[1],self.head,self.v_size).transpose(1,2) # v: (batch_size,head,seq_len,v_size)
+        z=torch.matmul(attn,v) # z: (batch_size,head,seq_len,v_size)
         return z
 
 if __name__=='__main__':
@@ -36,5 +53,6 @@ if __name__=='__main__':
 
     # 多头注意力
     multihead=MultiHeadAttention(emb_size=128,q_k_size=256,v_size=512,head=8)
-    multihead_result=multihead.forward(x_q=emb_result,x_k_v=emb_result,attn_mask=None)
+    attn_mask=torch.zeros((1,de_ids_tensor.size()[0],de_ids_tensor.size()[0])) # batch中每个样本对应1个注意力矩阵
+    multihead_result=multihead.forward(x_q=emb_result,x_k_v=emb_result,attn_mask=attn_mask)
     print('multihead_result:', multihead_result.size())
